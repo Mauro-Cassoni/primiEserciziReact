9fc67b1367fdafbfeda51f83205318454bc17dbd
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2024-05-10 13:01:08 +0200

    style

--- 2-DarkMode/src/components/card/Card.tsx
+++ 2-DarkMode/src/components/card/Card.tsx
@@ -7,11 +7,11 @@ interface iProp extends iCard {
 
 export default function Card({ remove, ...card }: iProp): JSX.Element {
     return (
-        <div className="card text-center w-72 rounded bg-white p-3 shadow-md shadow-slate-500">
+        <div className="card text-center w-72 rounded shadow-md shadow-slate-500 p-3">
             <h2 className="text-xl font-bold">{card.title}</h2>
-            <div className="mb-3 border-b-2 border-b-blue-500 w-1/3 mx-auto"></div>
+            <div className="undertext mb-3 border-b-2 w-1/3 mx-auto"></div>
             <p>{card.text}</p>
-            <button onClick={() => remove(card.id)}>x</button>
+            <button className="remove px-2 py-1 rounded m-1" onClick={() => remove(card.id)}>x</button>
         </div>
     )
 }
