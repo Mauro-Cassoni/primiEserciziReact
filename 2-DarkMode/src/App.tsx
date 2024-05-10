f08a7f51a66a8ad585241f2a35096151fb36270c
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2024-05-10 12:08:36 +0200

    app.tsx

--- 2-DarkMode/src/App.tsx
+++ 2-DarkMode/src/App.tsx
@@ -1,24 +1,45 @@
 import './App.scss'
 import Button from './components/button/Button'
 import { cards as cardArray } from './arrays/Array'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { iCard } from './interfaces/iCard'
 import Card from './components/card/Card'
 
+const getThemeFromLocalStorage = () => {
+  if (localStorage.getItem('theme')) {
+    return localStorage.getItem('theme');
+  } else {
+    return "light-mode";
+  }
+};
+
 function App() {
   const [posts, setPosts] = useState<iCard[]>(cardArray);
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+  const changeTheme = () => {
+    if (theme === "light-mode") {
+      setTheme("dark-mode")
+    } else {
+      setTheme("light-mode")
+    }
+  }
+
+  useEffect(() => {
+    if (theme !== null) {
+      document.documentElement.className = theme;
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme])
+
 
   const remove = (id: number) => {
     setPosts(cardArray.filter(p => p.id !== id))
   }
 
-  const handleClick = () => {
-    console.log(cardArray);
-  }
-
   return (
     <div className='flex flex-col items-center justify-center'>
-      <Button text="CHANGE" onClick={handleClick} ></Button>
+      <Button text="CHANGE" onClick={changeTheme} ></Button>
       <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4'>
         {
           posts.map(post => (
